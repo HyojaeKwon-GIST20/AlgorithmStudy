22fe9f3175c3ee9bed2b54b3e7e6beb532a93160
HyojaeKwon-GIST20 <khj010909@gm.gist.ac.kr>
2021-07-15 00:26:29 +0900

    chapter 8

---
+++ chapter8/8-5.py
@@ -0,0 +1,16 @@
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+
+dp_table = [0]*30001
+
+for i in range(2,n+1):
+    dp_table[i]=dp_table[i-1]+1
+    if dp_table[i] % 2 == 0:
+        dp_table[i] = min(dp_table[i],dp_table[i//2]+1)
+    if dp_table[i] % 3 == 0:
+        dp_table[i] = min(dp_table[i],dp_table[i//3]+1)
+    if dp_table[i] % 5 == 0:
+        dp_table[i] = min(dp_table[i],dp_table[i//5]+1)
+print(dp_table[n])
