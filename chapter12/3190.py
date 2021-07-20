73df5db48c16ac8366c3da9976e1b76bdd6f436e
HyojaeKwon-GIST20 <khj010909@gm.gist.ac.kr>
2021-07-20 14:59:41 +0900

    snake

---
+++ chapter12/3190.py
@@ -0,0 +1,30 @@
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+graph = [[False] * n for _ in range(n)]
+
+def play(apple,move):
+    count = 0
+    while True:
+        
+
+
+
+
+
+
+
+
+
+
+k = int(read())
+apple = []
+for _ in range(k):
+    apple.append(list(map(int,read().split())))
+
+l = int(read())
+move=[]
+for _ in range(l):
+    move.append(list(map(str,read().split())))
+
