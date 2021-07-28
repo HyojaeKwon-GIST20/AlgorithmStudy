6378a100c2b1bc2fa0ef3932c1e03979ba3cb422
HyojaeKwon-GIST20 <khj010909@gm.gist.ac.kr>
2021-07-28 17:45:35 +0900

    2579.py

--- BOJ/2579.py
+++ BOJ/2579.py
@@ -1,7 +1,27 @@
 import sys
 read = sys.stdin.readline
 
-dp = []*301
-
-k = read()
+k = int(read())
+dp=[]
+dp.append(0)
 for i in range(k):
+    dp.append(int(read()))
+
+anp = []
+anp.append(0)
+while True:
+    anp.append(dp[1])
+    if k == 1:
+        break
+    anp.append(max(dp[2],dp[2]+dp[1]))
+    if k == 2:
+        break
+    anp.append(max(dp[3]+dp[2],dp[3]+dp[1]))
+    if k >= 3:
+        break
+if k <= 3:
+    print(anp.pop())
+else:
+    for i in range(4,k+1):
+        anp.append(max(dp[i]+anp[i-2],dp[i]+dp[i-1]+anp[i-3]))
+    print(anp.pop())
